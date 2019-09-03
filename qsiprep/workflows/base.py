54c69aa69be5aa8c0cb1bfb5b34bf5f149e04e10
Matt Cieslak <mattcieslak@gmail.com>
2019-09-03 15:04:18 -0400

    [FIX] reconnect datasink and downgrade nipype (#21)
    * pass optimization data to finalize workflow
    
    * pin nipype to 1.1.9

--- qsiprep/workflows/base.py
+++ qsiprep/workflows/base.py
@@ -707,7 +707,8 @@ to workflows in *qsiprep*'s documentation]\
                     ('outputnode.dwi_mask', 'inputnode.dwi_mask'),
                     ('outputnode.hmc_xforms', 'inputnode.hmc_xforms'),
                     ('outputnode.fieldwarps', 'inputnode.fieldwarps'),
-                    ('outputnode.itk_b0_to_t1', 'inputnode.itk_b0_to_t1')
+                    ('outputnode.itk_b0_to_t1', 'inputnode.itk_b0_to_t1'),
+                    ('outputnode.hmc_optimization_data', 'inputnode.hmc_optimization_data')
                     ])
         ])
 
