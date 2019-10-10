60d8f950d9249031e1f31fba6ea6f126e6bc164e
Matt Cieslak <mattcieslak@gmail.com>
2019-10-10 11:51:04 -0400

    get pypi to work

--- wrapper/setup.py
+++ wrapper/setup.py
@@ -15,7 +15,7 @@ def main():
 
     setup(
         name=info['__packagename__'],
-        version='0.4.1',
+        version=info['__version__'],
         description=info['__description__'],
         long_description=info['__longdesc__'],
         author=info['__author__'],
