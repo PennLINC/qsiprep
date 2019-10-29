74329ec6708d57d0cb8a598f7a5f267cfc5eb509
WillForan <willforan@gmail.com>
2019-10-29 16:26:11 -0400

    EHN: add option tests and make ouput res required (fix issue #37)

--- qsiprep/cli/run.py
+++ qsiprep/cli/run.py
@@ -221,7 +221,7 @@ def get_parser():
     g_conf.add_argument(
         '--output-resolution', '--output_resolution',
         action='store',
-        required=False,
+        required=True,
         type=float,
         help='the isotropic voxel size in mm the data will be resampled to '
         'after preprocessing. If set to a lower value than the original voxel '
@@ -419,21 +419,9 @@ def get_parser():
     return parser
 
 
-def main():
-    """Entry point"""
-    from nipype import logging as nlogging
-    from multiprocessing import set_start_method, Process, Manager
-    from ..viz.reports import generate_reports
-    from ..utils.bids import write_derivative_description, validate_input_dir
-
-    try:
-        set_start_method('forkserver')
-    except RuntimeError:
-        pass
-
-    warnings.showwarning = _warn_redirect
-    opts = get_parser().parse_args()
-
+def validate_bids(opts):
+    """Validate bids unless opts say otherwise"""
+    from ..utils.bids import validate_input_dir
     exec_env = os.name
 
     # special variable set in the container
@@ -449,9 +437,15 @@ def main():
         print("Making sure the input data is BIDS compliant (warnings can be ignored in most "
               "cases).")
         validate_input_dir(exec_env, opts.bids_dir, opts.participant_label)
+        return True
+    return False
 
+
+def set_freesurfer_license(opts):
+    """Set FS_LICENSE environment variable"""
     # FreeSurfer license
-    default_license = str(Path(os.getenv('FREESURFER_HOME')) / 'license.txt')
+    # if qsiprep's current directory has a license.txt file, this will use that
+    default_license = str(Path(os.getenv('FREESURFER_HOME', '')) / 'license.txt')
     # Precedence: --fs-license-file, $FS_LICENSE, default_license
     license_file = opts.fs_license_file or Path(os.getenv('FS_LICENSE', default_license))
     if not license_file.exists():
@@ -461,6 +455,27 @@ license file at several paths, in this order: 1) command line argument ``--fs-li
 2) ``$FS_LICENSE`` environment variable; and 3) the ``$FREESURFER_HOME/license.txt`` path. Get it \
 (for free) by registering at https://surfer.nmr.mgh.harvard.edu/registration.html""")
     os.environ['FS_LICENSE'] = str(license_file.resolve())
+    return os.environ['FS_LICENSE']
+
+
+def main():
+    """Entry point"""
+    from nipype import logging as nlogging
+    from multiprocessing import set_start_method, Process, Manager
+    from ..viz.reports import generate_reports
+    from ..utils.bids import write_derivative_description
+
+    try:
+        set_start_method('forkserver')
+    except RuntimeError:
+        pass
+
+    warnings.showwarning = _warn_redirect
+    opts = get_parser().parse_args()
+
+    validate_bids(opts)
+
+    set_freesurfer_license(opts)
 
     # Retrieve logging level
     log_level = int(max(25 - 5 * opts.verbose_count, logging.DEBUG))
@@ -590,6 +605,7 @@ license file at several paths, in this order: 1) command line argument ``--fs-li
             errno = 1
         else:
             raise
+
 
 def build_qsiprep_workflow(opts, retval):
     """
---
+++ tests/opts_tests.py
@@ -0,0 +1,73 @@
+import pytest
+import os
+from qsiprep.cli.run import set_freesurfer_license, validate_bids, get_parser
+
+base_args = "--bids-dir bids --output-dir out --analysis-level participant --output_resolution 2.3"
+
+
+def test_required():
+    """error if we dont have necessary args"""
+    args = base_args.split(' ')
+
+    # all we need
+    opts = get_parser().parse_args(args)
+
+    # none
+    with pytest.raises(SystemExit) as pa_fail:
+        get_parser().parse_args([])
+    assert pa_fail.value.code == 2
+
+    # missing one of any of the base args
+    # all base args are pairs of 2
+    for i in [0, 2, 4, 6]:
+        part_args = args[0:i] + args[(i+2):]
+        with pytest.raises(SystemExit) as pa_fail:
+            get_parser().parse_args(part_args)
+        assert pa_fail.value.code == 2
+
+
+def test_set_freesurfer_license(tmpdir):
+    """test setting, precedence, and error if DNE"""
+    # create temp file
+    lic1 = tmpdir.join("license.txt")
+    lic2 = tmpdir.join("license2.txt")
+    lic3 = tmpdir.join("license3.txt")
+    for lic in [lic1, lic2, lic3]:
+        lic.write('not a real license')
+    opts = get_parser().parse_args(base_args.split(' '))
+
+    # no FS file, throw an error
+    if os.getenv('FREESURFER_HOME'):
+        del os.environ['FREESURFER_HOME']
+    if os.getenv('FS_LICENSE'):
+        del os.environ['FS_LICENSE']
+    with pytest.raises(RuntimeError):
+        fs = set_freesurfer_license(opts)
+
+    # using FRESURFER_HOME
+    os.environ['FREESURFER_HOME'] = tmpdir.__str__()
+    set_freesurfer_license(opts)
+    assert os.getenv('FS_LICENSE') == f'{lic1}'
+
+    # set directly -- check function output
+    os.environ['FS_LICENSE'] = f'{lic2}'
+    assert set_freesurfer_license(opts) == f'{lic2}'
+
+    # using command line switch overwrites other options
+    fsarg = f"{base_args} --fs-license-file {lic3}"
+    opts = get_parser().parse_args(fsarg.split(' '))
+    set_freesurfer_license(opts)
+    assert os.getenv('FS_LICENSE') == f'{lic3}'
+
+
+@pytest.mark.parametrize("will_validate,opts_str", (
+  (True, base_args),                                           # run if base args
+  (False, base_args + " --skip_bids_validation"),              # not if skipped
+  (False, base_args + " --recon-only"),                        # or recon all
+  (False, base_args + " --skip_bids_validation --recon-only")  # or both
+))
+def test_validate_bids(monkeypatch, opts_str, will_validate):
+    # from ..utils.bids import validate_input_dir
+    monkeypatch.setattr("qsiprep.utils.bids.validate_input_dir", lambda *kargs: True)
+    opts = get_parser().parse_args(opts_str.split(' '))
+    assert will_validate == validate_bids(opts)
