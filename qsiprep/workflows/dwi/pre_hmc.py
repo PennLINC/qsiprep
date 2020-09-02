80060d547f0e9d5458a0284172d66248f9c249d8
Matt Cieslak <mattcieslak@gmail.com>
2020-09-02 17:24:39 -0400

    add back qc closes #138

--- qsiprep/workflows/dwi/pre_hmc.py
+++ qsiprep/workflows/dwi/pre_hmc.py
@@ -224,8 +224,16 @@ def init_dwi_pre_hmc_wf(scan_groups,
             (pm_bias, outputnode, [
                 ('out', 'bias_images')]),
             (pm_raw_images, raw_rpe_concat, [('out', 'in_files')]),
-            (raw_rpe_concat, outputnode, [('out_file', 'raw_concatenated')])
+            (raw_rpe_concat, outputnode, [('out_file', 'raw_concatenated')]),
+
+            # Connect to the QC calculator
+            (raw_rpe_concat, qc_wf, [('out_file', 'inputnode.dwi_file')]),
+            (rpe_concat, qc_wf, [
+                ('out_bval', 'inputnode.bval_file'),
+                ('out_bvec', 'inputnode.bvec_file')]),
+            (qc_wf, outputnode, [('outputnode.qc_summary', 'qc_file')])
         ])
+        
         workflow.__postdesc__ = "Both groups were then merged into a single file, as required " \
                                 "for the FSL workflows. "
         return workflow
