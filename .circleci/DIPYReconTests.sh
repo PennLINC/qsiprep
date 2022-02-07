040d7dba96b1de1f2f719e4b0f3359f76358de45
Matt Cieslak <mattcieslak@gmail.com>
2022-02-07 12:44:29 -0500

    split recon tests

---
+++ .circleci/DIPYReconTests.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cat << DOC
+
+Reconstruction workflow tests
+=============================
+
+All supported reconstruction workflows get tested
+
+This tests the following features:
+ - Blip-up + Blip-down DWI series for TOPUP/Eddy
+ - Eddy is run on a CPU
+ - Denoising is skipped
+ - A follow-up reconstruction using the dsi_studio_gqi workflow
+
+Inputs:
+-------
+
+ - qsiprep single shell results (data/DSDTI_fmap)
+ - qsiprep multi shell results (data/DSDTI_fmap)
+
+DOC
+
+source ./get_data.sh
+TESTDIR=${PWD}
+get_config_data ${TESTDIR}
+get_bids_data ${TESTDIR} singleshell_output
+get_bids_data ${TESTDIR} multishell_output
+CFG=${TESTDIR}/data/nipype.cfg
+EDDY_CFG=${TESTDIR}/data/eddy_config.json
+export FS_LICENSE=${TESTDIR}/data/license.txt
+
+# Test dipy_mapmri
+TESTNAME=dipy_mapmri_test
+setup_dir ${TESTDIR}/${TESTNAME}
+TEMPDIR=${TESTDIR}/${TESTNAME}/work
+OUTPUT_DIR=${TESTDIR}/${TESTNAME}/derivatives
+BIDS_INPUT_DIR=${TESTDIR}/data/multishell_output/qsiprep
+
+qsiprep-docker -i pennbbl/qsiprep:latest \
+	-e qsiprep_DEV 1 -u $(id -u) \
+	--config ${CFG} ${PATCH} -w ${TEMPDIR} \
+	 ${BIDS_INPUT_DIR} ${OUTPUT_DIR} \
+	 participant \
+	 --recon-input ${BIDS_INPUT_DIR} \
+	 --sloppy \
+	 --recon-spec dipy_mapmri \
+	 --recon-only \
+	 --mem_mb 4096 \
+	 --nthreads 1 -vv
+
+
+
