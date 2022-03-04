8001cc9a395f695f5f7bc1d031c9bbb485c65125
36000 <jk6.28@outlook.com>
2022-03-04 15:07:02 -0800

    first draft of full pyAFQ pipeline

--- qsiprep/interfaces/pyafq.py
+++ qsiprep/interfaces/pyafq.py
@@ -10,30 +10,30 @@ Interface for running a PyAFQ workflow
 """
 import os
 import os.path as op
-from pkg_resources import resource_filename as pkgr
-import nibabel as nb
-from pyafq import
-import numpy as np
+
+from AFQ.api.participant import ParticipantAFQ
+from AFQ.definitions.mask import MaskFile
+from AFQ.definitions.mapping import ItkMap
+from AFQ.utils.bin import parse_qsiprep_params_dict
+
 from nipype import logging
 from nipype.utils.filemanip import fname_presuffix
 from nipype.interfaces.base import (
-    traits, TraitedSpec, BaseInterfaceInputSpec, File, SimpleInterface, isdefined
+    traits, TraitedSpec, BaseInterfaceInputSpec, File, SimpleInterface
 )
 
 
 LOGGER = logging.getLogger('nipype.interface')
-TAU_DEFAULT = 1. / (4 * np.pi**2)
 
 
 class PyAFQInputSpec(BaseInterfaceInputSpec):
-    b0_threshold = traits.Int(50, usedefault=True)
     bval_file = File(exists=True, mandatory=True)
     bvec_file = File(exists=True, mandatory=True)
     dwi_file = File(exists=True, mandatory=True)
-    mask_file = File(exists=True)
-    big_delta = traits.Either(None, traits.Float(), usedefault=True)
-    little_delta = traits.Either(None, traits.Float(), usedefault=True)
-    b0_threshold = traits.CFloat(50, usedefault=True)
+    mask_file = File(exists=True, mandatory=True)
+    itk_file = File(exists=True, mandatory=True)
+    kwargs = traits.Dict(exists=True, mandatory=True)
+    trk_file = traits.Either(None, File(exists=True))
 
 
 class PyAFQOutputSpec(TraitedSpec):
@@ -47,26 +47,38 @@ class PyAFQRecon(SimpleInterface):
     def _run_interface(self, runtime):
 
         # shim the expected inputs
-        # shim_dir = op.join(runtime.cwd, "study/subject")
-        # os.makedirs(shim_dir)
-        # bval_file = fname_presuffix(self.inputs.bval_file,
-        #                             newpath=shim_dir)
-        # bvec_file = fname_presuffix(self.inputs.bvec_file,
-        #                             newpath=shim_dir)
-        # dwi_file = fname_presuffix(self.inputs.dwi_file,
-        #                             newpath=shim_dir)
-        # mask_file = fname_presuffix(self.inputs.mask_file,
-        #                             newpath=shim_dir)
-        # os.symlink(self.inputs.bval_file, bval_file)
-        # os.symlink(self.inputs.bvec_file, bvec_file)
-        # os.symlink(self.inputs.dwi_file, dwi_file)
-        # os.symlink(self.inputs.mask_file, mask_file)
+        shim_dir = op.join(runtime.cwd, "study/subject")
+        os.makedirs(shim_dir)
+        bval_file = fname_presuffix(self.inputs.bval_file,
+                                    newpath=shim_dir)
+        bvec_file = fname_presuffix(self.inputs.bvec_file,
+                                    newpath=shim_dir)
+        dwi_file = fname_presuffix(self.inputs.dwi_file,
+                                   newpath=shim_dir)
+        mask_file = fname_presuffix(self.inputs.mask_file,
+                                    newpath=shim_dir)
+        trk_file = fname_presuffix(self.inputs.trk_file,
+                                   newpath=shim_dir)
+        itk_file = fname_presuffix(self.inputs.itk_file,
+                                   newpath=shim_dir)
+        os.symlink(self.inputs.bval_file, bval_file)
+        os.symlink(self.inputs.bvec_file, bvec_file)
+        os.symlink(self.inputs.dwi_file, dwi_file)
+        os.symlink(self.inputs.mask_file, mask_file)
 
+        brain_mask_definition = MaskFile(path=mask_file)
+        itk_map = ItkMap(warp_path=itk_file)
+        output_dir = shim_dir + "/PYAFQ/"
 
-        self._results['directions_image'] = shim_dir + "/AMICO/NODDI/FIT_dir.nii.gz"
-        self._results['icvf_image'] = shim_dir + "/AMICO/NODDI/FIT_ICVF.nii.gz"
-        self._results['od_image'] = shim_dir + "/AMICO/NODDI/FIT_OD.nii.gz"
-        self._results['isovf_image'] = shim_dir + "/AMICO/NODDI/FIT_ISOVF.nii.gz"
-        self._results['config_file'] = shim_dir + "/AMICO/NODDI/config.pickle"
+        kwargs = parse_qsiprep_params_dict(self.inputs)
+
+        myafq = ParticipantAFQ(
+            dwi_file, bval_file, bvec_file, output_dir,
+            import_tract=trk_file,
+            brain_mask_definition=brain_mask_definition,
+            mapping_definition=itk_map,
+            **kwargs)
+
+        self._results['profiles_csv'] = myafq.profiles
 
         return runtime
