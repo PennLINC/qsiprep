8b58711406f83d79af4583954a9104d499ee9fe7
36000 <jk6.28@outlook.com>
2022-04-06 11:34:01 -0700

    updated interfact pyafq

--- qsiprep/interfaces/pyafq.py
+++ qsiprep/interfaces/pyafq.py
@@ -14,16 +14,45 @@ import os.path as op
 from AFQ.api.participant import ParticipantAFQ
 from AFQ.definitions.mask import MaskFile
 from AFQ.definitions.mapping import ItkMap
-from AFQ.utils.bin import parse_qsiprep_params_dict
+import AFQ.utils.bin as afb
 
 from nipype import logging
 from nipype.utils.filemanip import fname_presuffix
 from nipype.interfaces.base import (
-    traits, TraitedSpec, BaseInterfaceInputSpec, File, SimpleInterface
+    traits, TraitedSpec, BaseInterfaceInputSpec, File, SimpleInterface, isdefined
 )
 
 
 LOGGER = logging.getLogger('nipype.interface')
+
+
+def parse_qsiprep_params_dict(params_dict):
+    arg_dict = afb.func_dict_to_arg_dict()
+    kwargs = {}
+
+    special_args = {
+        "CLEANING": "clean_params",
+        "SEGMENTATION": "segmentation_params",
+        "TRACTOGRAPHY": "tracking_params"}
+
+    for section, args in arg_dict.items():
+        if section == "AFQ_desc":
+            continue
+        for arg, arg_info in args.items():
+            if arg in special_args.keys():
+                kwargs[special_args[arg]] = {}
+                for actual_arg in arg_info.keys():
+                    if actual_arg in params_dict:
+                        kwargs[special_args[arg]][actual_arg] = afb.toml_to_val(
+                            params_dict[actual_arg])
+            else:
+                if arg in params_dict:
+                    kwargs[arg] = afb.toml_to_val(params_dict[arg])
+
+    for ignore_param in afb.qsi_prep_ignore_params:
+        kwargs.pop(ignore_param, None)
+
+    return kwargs
 
 
 class PyAFQInputSpec(BaseInterfaceInputSpec):
@@ -37,7 +66,7 @@ class PyAFQInputSpec(BaseInterfaceInputSpec):
 
 
 class PyAFQOutputSpec(TraitedSpec):
-    out_directory = File()
+    afq_dir = traits.Directory()
 
 
 class PyAFQRecon(SimpleInterface):
@@ -65,12 +94,17 @@ class PyAFQRecon(SimpleInterface):
         os.symlink(self.inputs.bvec_file, bvec_file)
         os.symlink(self.inputs.dwi_file, dwi_file)
         os.symlink(self.inputs.mask_file, mask_file)
+        os.symlink(self.inputs.itk_file, itk_file)
+        if self.inputs.trk_file and isdefined(self.inputs.trk_file):
+            os.symlink(self.inputs.trk_file, trk_file)
+        else:
+            trk_file = None
 
         brain_mask_definition = MaskFile(path=mask_file)
         itk_map = ItkMap(warp_path=itk_file)
         output_dir = shim_dir + "/PYAFQ/"
 
-        kwargs = parse_qsiprep_params_dict(self.inputs)
+        kwargs = parse_qsiprep_params_dict(self.inputs.kwargs)
 
         myafq = ParticipantAFQ(
             dwi_file, bval_file, bvec_file, output_dir,
@@ -78,7 +112,8 @@ class PyAFQRecon(SimpleInterface):
             brain_mask_definition=brain_mask_definition,
             mapping_definition=itk_map,
             **kwargs)
+        myafq.export_all() # TODO: add this to parameter file
 
-        self._results['profiles_csv'] = myafq.profiles
+        self._results['afq_dir'] = output_dir
 
         return runtime
