da581733eca6426067295152ff9946408c30c210
Matt Cieslak <mattcieslak@gmail.com>
2022-10-10 12:06:23 -0400

    start reports [skip ci]

--- qsiprep/niworkflows/viz/utils.py
+++ qsiprep/niworkflows/viz/utils.py
@@ -399,6 +399,60 @@ def plot_registration(anat_nii, div_id, plot_params=None,
     return out_files
 
 
+def get_canvas()
+
+def plot_rpe_sdc(blip_up_nii, blip_down_nii, blip_up_nii_corrected, blip_down_nii_corrected,
+                 div_id,
+                 order=('z', 'x', 'y'), cuts=None,
+                 estimate_brightness=False, label=None, lowb_contour=None,
+                 compress='auto', overlay=None, overlay_params=None):
+    """
+    Plots the before and after of reverse phase encoding susceptibility distortion correction.
+    Default order is: axial, coronal, sagittal
+    """
+    plot_params = plot_params or {}
+
+    # Use default MNI cuts if none defined
+    if cuts is None:
+        raise NotImplementedError
+
+    # Do the low-b image first
+    out_files = []
+    if estimate_brightness:
+        plot_params = robust_set_limits(
+            lowb_nii.get_fdata(dtype='float32').reshape(-1),
+            plot_params)
+    # Plot each cut axis for low-b
+    for i, mode in enumerate(list(order)):
+        plot_params['display_mode'] = mode
+        plot_params['cut_coords'] = cuts[mode]
+        if i == 0:
+            plot_params['title'] = label + ": low-b"
+        else:
+            plot_params['title'] = None
+
+        # Generate nilearn figure
+        display = plot_anat(lowb_nii, **plot_params)
+
+        svg = extract_svg(display, compress=compress)
+        display.close()
+
+        # Find and replace the figure_1 id.
+        xml_data = etree.fromstring(svg)
+        find_text = etree.ETXPath("//{%s}g[@id='figure_1']" % SVGNS)
+        find_text(xml_data)[0].set('id', '%s-%s-%s' % (div_id, mode, uuid4()))
+
+        svg_fig = SVGFigure()
+        svg_fig.root = xml_data
+        out_files.append(svg_fig)
+
+        svg_fig = SVGFigure()
+        svg_fig.root = xml_data
+        out_files.append(svg_fig)
+
+    return out_files
+
+
 def plot_denoise(lowb_nii, highb_nii, div_id, plot_params=None, highb_plot_params=None,
                  order=('z', 'x', 'y'), cuts=None,
                  estimate_brightness=False, label=None, lowb_contour=None,
