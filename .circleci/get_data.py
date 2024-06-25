87f020696033df46a5f7b2f094356ba5b77b5df1
Taylor Salo <salot@pennmedicine.upenn.edu>
2024-06-25 12:23:17 -0400

    [RF] Run integration tests with pytest (#763)

---
+++ .circleci/get_data.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Download test data."""
+import sys
+
+from qsiprep.tests.utils import download_test_data
+
+if __name__ == "__main__":
+    data_dir = sys.argv[1]
+    dset = sys.argv[2]
+    download_test_data(dset, data_dir)
