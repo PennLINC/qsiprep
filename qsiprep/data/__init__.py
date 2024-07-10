2dbd76f0d1c8c5e8d6871599d1443ce74f60533b
Taylor Salo <tsalo006@fiu.edu>
2024-07-10 14:14:43 -0400

    Run black.

--- qsiprep/data/__init__.py
+++ qsiprep/data/__init__.py
@@ -35,7 +35,7 @@ try:  # Prefer stdlib so Sphinx can link to authoritative documentation
 except ImportError:
     from importlib_resources.abc import Traversable
 
-__all__ = ['load']
+__all__ = ["load"]
 
 
 class Loader:
@@ -127,19 +127,19 @@ class Loader:
         directory.
         """
         top_level = sorted(
-            os.path.relpath(p, self.files) + '/'[: p.is_dir()]
+            os.path.relpath(p, self.files) + "/"[: p.is_dir()]
             for p in self.files.iterdir()
-            if p.name[0] not in ('.', '_') and p.name != 'tests'
+            if p.name[0] not in (".", "_") and p.name != "tests"
         )
         doclines = [
-            f'Load package files relative to ``{self._anchor}``.',
-            '',
-            'This package contains the following (top-level) files/directories:',
-            '',
-            *(f'* ``{path}``' for path in top_level),
+            f"Load package files relative to ``{self._anchor}``.",
+            "",
+            "This package contains the following (top-level) files/directories:",
+            "",
+            *(f"* ``{path}``" for path in top_level),
         ]
 
-        return '\n'.join(doclines)
+        return "\n".join(doclines)
 
     def readable(self, *segments) -> Traversable:
         """Provide read access to a resource through a Path-like interface.
