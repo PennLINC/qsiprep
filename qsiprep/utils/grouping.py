1c740aee09cce0e765819fe85e98e6a46e1c688b
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-02-20 14:01:07 -0500

    FFS

--- qsiprep/utils/grouping.py
+++ qsiprep/utils/grouping.py
@@ -1003,7 +1003,7 @@ def group_for_eddy(all_dwi_fmap_groups):
     ...       'epi': ['.../mixed_fmaps/sub-1/fmap/sub-1_dir-AP_run-2_epi.nii.gz']},
     ...       'dwi_series_pedir': 'j-',
     ...       'concatenated_bids_name': 'sub-1_dir-PA_run-2'}]
-    >>> group_for_eddy(dwi_groups) # doctest: +NORMALIZE_WHITESPACE
+    >>> pprint(group_for_eddy(dwi_groups)) # doctest: +NORMALIZE_WHITESPACE
     ([{'concatenated_bids_name': 'sub-1',
        'dwi_series': ['.../mixed_fmaps/sub-1/dwi/sub-1_dir-AP_run-1_dwi.nii.gz'],
        'dwi_series_pedir': 'j',
