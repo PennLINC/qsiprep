PennLINC/qsiprep

commit 69be22653ac689e0f8ff5b23a306fea0c650de06

Crop images before plotting (#997)
* Crop images before plotting.

* Remove old notebook.

* Update utils.py

* Update utils.py

* Work around nilearn bug.

qsiprep/viz/utils.py
@@ -1,7 +1,8 @@
 """Visualization utilities."""
 
+import numpy as np
 from lxml import etree
-from nilearn.plotting import plot_anat
+from nilearn import image, plotting
 from niworkflows.viz.utils import SVGNS, extract_svg, robust_set_limits, uuid4
 from svgutils.transform import SVGFigure
 
@@ -44,6 +45,12 @@ def plot_denoise(
             lowb_nii.get_fdata(dtype='float32').reshape(-1), plot_params
         )
     # Plot each cut axis for low-b
+    lowb_nii_data = lowb_nii.get_fdata(dtype='float32')
+    if np.all(lowb_nii_data <= 1e-8):
+        lowb_nii_cropped = lowb_nii
+    else:
+        lowb_nii_cropped = image.crop_img(lowb_nii)
+
     for i, mode in enumerate(list(order)):
         plot_params['display_mode'] = mode
         plot_params['cut_coords'] = cuts[mode]
@@ -53,7 +60,7 @@ def plot_denoise(
             plot_params['title'] = None
 
         # Generate nilearn figure
-        display = plot_anat(lowb_nii, **plot_params)
+        display = plotting.plot_anat(lowb_nii_cropped, **plot_params)
         if lowb_contour is not None:
             display.add_contours(lowb_contour, linewidths=1)
 
@@ -74,6 +81,13 @@ def plot_denoise(
         highb_plot_params = robust_set_limits(
             highb_nii.get_fdata(dtype='float32').reshape(-1), highb_plot_params
         )
+
+    highb_nii_data = highb_nii.get_fdata(dtype='float32')
+    if np.all(highb_nii_data <= 1e-8):
+        highb_nii_cropped = highb_nii
+    else:
+        highb_nii_cropped = image.crop_img(highb_nii)
+
     for i, mode in enumerate(list(order)):
         highb_plot_params['display_mode'] = mode
         highb_plot_params['cut_coords'] = cuts[mode]
@@ -83,7 +97,7 @@ def plot_denoise(
             highb_plot_params['title'] = None
 
         # Generate nilearn figure
-        display = plot_anat(highb_nii, **highb_plot_params)
+        display = plotting.plot_anat(highb_nii_cropped, **highb_plot_params)
         if highb_contour is not None:
             display.add_contours(highb_contour, linewidths=1)
 
@@ -125,6 +139,12 @@ def plot_acpc(
         )
 
     # Plot each cut axis for low-b
+    acpc_registered_img_data = acpc_registered_img.get_fdata(dtype='float32')
+    if np.all(acpc_registered_img_data <= 1e-8):
+        acpc_registered_img_cropped = acpc_registered_img
+    else:
+        acpc_registered_img_cropped = image.crop_img(acpc_registered_img)
+
     for i, mode in enumerate(list(order)):
         plot_params['display_mode'] = mode
         plot_params['cut_coords'] = [-20.0, 0.0, 20.0]
@@ -134,7 +154,7 @@ def plot_acpc(
             plot_params['title'] = None
 
         # Generate nilearn figure
-        display = plot_anat(acpc_registered_img, **plot_params)
+        display = plotting.plot_anat(acpc_registered_img_cropped, **plot_params)
         for _coord, axis in display.axes.items():
             axis.ax.axvline(0, lw=1)
             axis.ax.axhline(0, lw=1)

qsiprep/interfaces/fmap.py
@@ -18,11 +18,9 @@ import os.path as op
 from collections import defaultdict
 
 import nibabel as nb
-import nilearn.image as nim
-import nilearn.plotting as nip
 import numpy as np
 from lxml import etree
-from nilearn.image import concat_imgs, index_img, iter_img, load_img, math_img
+from nilearn import image, plotting
 from nipype import logging
 from nipype.interfaces import ants
 from nipype.interfaces.ants.resampling import ApplyTransformsInputSpec
@@ -142,7 +140,7 @@ class B0RPEFieldmap(SimpleInterface):
 
         image_list = []
         json_list = []
-        for imgnum, img in enumerate(iter_img(merged_b0s)):
+        for imgnum, img in enumerate(image.iter_img(merged_b0s)):
             # Save the conformed fmap and metadata
             output_fmap = fname_presuffix(
                 self.inputs.b0_file[0],
@@ -651,9 +649,9 @@ def phdiff2fmap(in_file, delta_te, newpath=None):
     #  GYROMAG_RATIO_H_PROTON_MHZ = 42.576
 
     out_file = fname_presuffix(in_file, suffix='_fmap', newpath=newpath)
-    image = nb.load(in_file)
-    data = image.get_fdata().astype(np.float32) / (2.0 * math.pi * delta_te)
-    nii = nb.Nifti1Image(data, image.affine, image.header)
+    img = nb.load(in_file)
+    data = img.get_fdata().astype(np.float32) / (2.0 * math.pi * delta_te)
+    nii = nb.Nifti1Image(data, img.affine, img.header)
     nii.set_data_dtype(np.float32)
     nii.to_filename(out_file)
     return out_file
@@ -841,7 +839,7 @@ def get_topup_inputs_from(
     b0_indices = np.flatnonzero(bvals < b0_threshold)
     if not b0_indices.size:
         raise RuntimeError('No b=0 images available for TOPUP from the dwi.')
-    dwi_nii = load_img(dwi_file)
+    dwi_nii = image.load_img(dwi_file)
     # Gather images from just the dwi series
     dwi_spec_lines, dwi_imain, dwi_report, _ = topup_inputs_from_4d_file(
         dwi_nii,
@@ -913,7 +911,7 @@ def load_epi_dwi_fieldmaps(fmap_list, b0_threshold):
         pth, fname, _ = split_filename(fmap_file)
         potential_bval_file = op.join(pth, fname) + '.bval'
         starting_index = len(original_files)
-        fmap_img = load_img(fmap_file)
+        fmap_img = image.load_img(fmap_file)
         image_series.append(fmap_img)
         num_images = 1 if fmap_img.ndim == 3 else fmap_img.shape[3]
         original_files += [fmap_file] * num_images
@@ -936,7 +934,7 @@ def load_epi_dwi_fieldmaps(fmap_list, b0_threshold):
             _b0_indices = np.arange(num_images) + starting_index
         b0_indices += _b0_indices.tolist()
 
-    concatenated_images = concat_imgs(image_series, auto_resample=True)
+    concatenated_images = image.concat_imgs(image_series, auto_resample=True)
     return concatenated_images, b0_indices, original_files
 
 
@@ -1010,7 +1008,7 @@ def topup_inputs_from_4d_file(
         spec_lines += [spec_line] * len(spec_b0_indices)
 
     # Load and subset the image
-    imain_nii = index_img(nii_file, selected_b0_indices)
+    imain_nii = image.index_img(nii_file, selected_b0_indices)
     report = topup_selection_to_report(
         selected_b0_indices, bids_origin_files, spec_lookup, image_source=image_source
     )
@@ -1077,8 +1075,8 @@ def add_epi_fmaps_to_dwi_b0s(epi_fmaps, b0_threshold, max_per_spec, dwi_spec_lin
         )
 
     # Add the epi b=0's to the dwi b=0's
-    topup_imain = concat_imgs(
-        [dwi_imain, index_img(fmap_imain, fmap_indices_to_add)], auto_resample=True
+    topup_imain = image.concat_imgs(
+        [dwi_imain, image.index_img(fmap_imain, fmap_indices_to_add)], auto_resample=True
     )
     topup_spec_lines = dwi_spec_lines + [fmap_spec_lines[idx] for idx in fmap_indices_to_add]
 
@@ -1235,7 +1233,7 @@ class ApplyScalingImages(SimpleInterface):
         scaled_dwi_images = []
         for dwi_file in self.inputs.dwi_files:
             scaled_dwi_file = fname_presuffix(dwi_file, newpath=runtime.cwd, suffix='_scaled')
-            math_img('a*b', a=dwi_file, b=dwi_files_to_scalings[dwi_file]).to_filename(
+            image.math_img('a*b', a=dwi_file, b=dwi_files_to_scalings[dwi_file]).to_filename(
                 scaled_dwi_file
             )
 
@@ -1331,8 +1329,8 @@ class PEPOLARReport(SimpleInterface):
         fa_up_corrected_img = nb.load(self.inputs.up_fa_corrected_image)
         fa_down_img = nb.load(self.inputs.down_fa_image)
         fa_down_corrected_img = nb.load(self.inputs.down_fa_corrected_image)
-        uncorrected_fa = nim.math_img('(a+b)/2', a=fa_up_img, b=fa_down_img)
-        corrected_fa = nim.math_img('(a+b)/2', a=fa_up_corrected_img, b=fa_down_corrected_img)
+        uncorrected_fa = image.math_img('(a+b)/2', a=fa_up_img, b=fa_down_img)
+        corrected_fa = image.math_img('(a+b)/2', a=fa_up_corrected_img, b=fa_down_corrected_img)
         compose_view(
             plot_fa_reg(
                 corrected_fa,
@@ -1395,7 +1393,8 @@ def plot_pepolar(
             blip_up_img.get_fdata(dtype='float32').reshape(-1), plot_params
         )
 
-    zeros_bg_img = nim.new_img_like(
+    seg_contour_img = image.crop_img(seg_contour_img)
+    zeros_bg_img = image.new_img_like(
         seg_contour_img, np.zeros(seg_contour_img.shape), copy_header=True
     )
 
@@ -1410,7 +1409,7 @@ def plot_pepolar(
             plot_params['title'] = None
 
         # Generate nilearn figure
-        display = nip.plot_anat(zeros_bg_img, **plot_params)
+        display = plotting.plot_anat(zeros_bg_img, **plot_params)
         display.add_overlay(blip_up_img, cmap='gray', **image_plot_params)
         display.add_contours(seg_contour_img, colors='b', linewidths=0.5)
 
@@ -1441,7 +1440,7 @@ def plot_pepolar(
             blip_down_plot_params['title'] = None
 
         # Generate nilearn figure
-        display = nip.plot_anat(zeros_bg_img, **blip_down_plot_params)
+        display = plotting.plot_anat(zeros_bg_img, **blip_down_plot_params)
         display.add_overlay(blip_down_img, cmap='gray', **image_blip_down_plot_params)
         display.add_contours(seg_contour_img, colors='b', linewidths=0.5)
         svg = extract_svg(display, compress=compress)
@@ -1485,7 +1484,8 @@ def plot_fa_reg(
         raise NotImplementedError
 
     out_files = []
-    zeros_bg_img = nim.new_img_like(
+    seg_contour_img = image.crop_img(seg_contour_img)
+    zeros_bg_img = image.new_img_like(
         seg_contour_img, np.zeros(seg_contour_img.shape), copy_header=True
     )
 
@@ -1500,7 +1500,7 @@ def plot_fa_reg(
             plot_params['title'] = None
 
         # Generate nilearn figure
-        display = nip.plot_anat(zeros_bg_img, **plot_params)
+        display = plotting.plot_anat(zeros_bg_img, **plot_params)
         display.add_overlay(fa_img, **image_plot_params)
         # display.add_contours(seg_contour_img, colors='b', linewidths=0.5)
 

commit 4bf5dec649a227aa0a1809a5498201848d7512af

Use same cropped FOV for before and after.
There was a bug in the reportlets that crop fields of view introduced by #997.

qsiprep/interfaces/denoise.py
@@ -12,7 +12,7 @@ import os
 import nibabel as nb
 import numpy as np
 import pandas as pd
-from nilearn.image import iter_img, load_img, threshold_img
+from nilearn.image import crop_img, iter_img, load_img, threshold_img
 from nipype import logging
 from nipype.interfaces.base import isdefined, traits
 from nipype.interfaces.mixins import reporting
@@ -121,7 +121,10 @@ class SeriesPreprocReport(reporting.ReportCapableInterface):
             contour_nii = load_img(self.inputs.mask)
         else:
             mask_nii = threshold_img(denoised_lowb_nii, 50)
-        cuts = cuts_from_bbox(contour_nii or mask_nii, cuts=self._n_cuts)
+
+        ref_nii = contour_nii or mask_nii
+        cuts = cuts_from_bbox(ref_nii, cuts=self._n_cuts)
+        _, crop_offset = crop_img(ref_nii, return_offset=True)
 
         # What image should be contoured?
         if field_nii is None:
@@ -144,6 +147,7 @@ class SeriesPreprocReport(reporting.ReportCapableInterface):
                 'moving-image',
                 estimate_brightness=True,
                 cuts=cuts,
+                crop_offset=crop_offset,
                 label='Raw Image',
                 lowb_contour=lowb_field_nii,
                 highb_contour=highb_field_nii,
@@ -155,6 +159,7 @@ class SeriesPreprocReport(reporting.ReportCapableInterface):
                 'fixed-image',
                 estimate_brightness=True,
                 cuts=cuts,
+                crop_offset=crop_offset,
                 label='Denoised',
                 lowb_contour=lowb_field_nii,
                 highb_contour=highb_field_nii,

qsiprep/interfaces/itk.py
@@ -14,6 +14,7 @@ import subprocess
 from mimetypes import guess_type
 
 import nibabel as nb
+import nilearn.image as nim
 import numpy as np
 import SimpleITK as sitk
 from dipy.core import geometry as geom
@@ -77,20 +78,28 @@ class ACPCReport(SimpleInterface):
 
     def _run_interface(self, runtime):
         out_report = runtime.cwd + '/ACPCReport.svg'
+        translation_img = nb.load(self.inputs.translation_image)
+        rigid_img = nb.load(self.inputs.rigid_img)
+        # combine images so crop offset captures both
+        sum_img = nim.math_image('a + b', a=translation_img, b=rigid_img)
+        _, crop_offset = nim.crop_img(sum_img, return_offset=True)
+
         # Call composer
         compose_view(
             plot_acpc(
-                nb.load(self.inputs.translation_image),
+                translation_img,
                 'moving-image',
                 estimate_brightness=True,
                 label='Original',
+                crop_offset=crop_offset,
                 compress=False,
             ),
             plot_acpc(
-                nb.load(self.inputs.rigid_image),
+                rigid_img,
                 'fixed-image',
                 estimate_brightness=True,
                 label='AC-PC',
+                crop_offset=crop_offset,
                 compress=False,
             ),
             out_file=out_report,

qsiprep/interfaces/mrtrix.py
@@ -11,7 +11,7 @@ import os
 
 import nibabel as nb
 import numpy as np
-from nilearn.image import load_img, threshold_img
+from nilearn.image import crop_img, load_img, threshold_img
 from nipype import logging
 from nipype.interfaces.base import (
     BaseInterfaceInputSpec,
@@ -562,7 +562,10 @@ class MRDeGibbs(SeriesPreprocReport, MRTrix3Base):
             contour_nii = load_img(self.inputs.mask)
         else:
             mask_nii = threshold_img(denoised_lowb_nii, 50)
-        cuts = cuts_from_bbox(contour_nii or mask_nii, cuts=self._n_cuts)
+
+        ref_nii = contour_nii or mask_nii
+        cuts = cuts_from_bbox(ref_nii, cuts=self._n_cuts)
+        _, crop_offset = crop_img(ref_nii, return_offset=True)
 
         diff_lowb_nii = nb.Nifti1Image(
             orig_lowb_nii.get_fdata() - denoised_lowb_nii.get_fdata(),
@@ -581,6 +584,7 @@ class MRDeGibbs(SeriesPreprocReport, MRTrix3Base):
                 'moving-image',
                 estimate_brightness=True,
                 cuts=cuts,
+                crop_offset=crop_offset,
                 label='De-Gibbs',
                 lowb_contour=None,
                 highb_contour=None,
@@ -592,6 +596,7 @@ class MRDeGibbs(SeriesPreprocReport, MRTrix3Base):
                 'fixed-image',
                 estimate_brightness=True,
                 cuts=cuts,
+                crop_offset=crop_offset,
                 label='Estimated Ringing',
                 lowb_contour=None,
                 highb_contour=None,

qsiprep/interfaces/tortoise.py
@@ -601,7 +601,10 @@ class Gibbs(SeriesPreprocReport, TORTOISECommandLine):
             contour_nii = nim.load_img(self.inputs.mask)
         else:
             mask_nii = nim.threshold_img(denoised_lowb_nii, 50)
-        cuts = cuts_from_bbox(contour_nii or mask_nii, cuts=self._n_cuts)
+
+        ref_nii = contour_nii or mask_nii
+        cuts = cuts_from_bbox(ref_nii, cuts=self._n_cuts)
+        _, crop_offset = nim.crop_img(ref_nii, return_offset=True)
 
         diff_lowb_nii = nb.Nifti1Image(
             orig_lowb_nii.get_fdata() - denoised_lowb_nii.get_fdata(),
@@ -620,6 +623,7 @@ class Gibbs(SeriesPreprocReport, TORTOISECommandLine):
                 'moving-image',
                 estimate_brightness=True,
                 cuts=cuts,
+                crop_offset=crop_offset,
                 label='De-Gibbs',
                 lowb_contour=None,
                 highb_contour=None,
@@ -631,6 +635,7 @@ class Gibbs(SeriesPreprocReport, TORTOISECommandLine):
                 'fixed-image',
                 estimate_brightness=True,
                 cuts=cuts,
+                crop_offset=crop_offset,
                 label='Estimated Ringing',
                 lowb_contour=None,
                 highb_contour=None,

qsiprep/viz/utils.py
@@ -15,6 +15,7 @@ def plot_denoise(
     highb_plot_params=None,
     order=('z', 'x', 'y'),
     cuts=None,
+    crop_offset=None,
     estimate_brightness=False,
     label=None,
     lowb_contour=None,
@@ -40,17 +41,14 @@ def plot_denoise(
 
     # Do the low-b image first
     out_files = []
+
+    # Plot each cut axis for low-b
     if estimate_brightness:
         plot_params = robust_set_limits(
             lowb_nii.get_fdata(dtype='float32').reshape(-1), plot_params
         )
-    # Plot each cut axis for low-b
-    lowb_nii_data = lowb_nii.get_fdata(dtype='float32')
-    if np.all(lowb_nii_data <= 1e-8):
-        lowb_nii_cropped = lowb_nii
-    else:
-        lowb_nii_cropped = image.crop_img(lowb_nii)
 
+    lowb_nii_cropped = lowb_nii if crop_offset is None else lowb_nii.slicer[crop_offset]
     for i, mode in enumerate(list(order)):
         plot_params['display_mode'] = mode
         plot_params['cut_coords'] = cuts[mode]
@@ -82,12 +80,7 @@ def plot_denoise(
             highb_nii.get_fdata(dtype='float32').reshape(-1), highb_plot_params
         )
 
-    highb_nii_data = highb_nii.get_fdata(dtype='float32')
-    if np.all(highb_nii_data <= 1e-8):
-        highb_nii_cropped = highb_nii
-    else:
-        highb_nii_cropped = image.crop_img(highb_nii)
-
+    highb_nii_cropped = highb_nii if crop_offset is None else highb_nii.slicer[crop_offset]
     for i, mode in enumerate(list(order)):
         highb_plot_params['display_mode'] = mode
         highb_plot_params['cut_coords'] = cuts[mode]
@@ -122,6 +115,7 @@ def plot_acpc(
     plot_params=None,
     order=('z', 'x', 'y'),
     cuts=None,
+    crop_offset=None,
     estimate_brightness=False,
     label=None,
     compress='auto',
@@ -139,12 +133,9 @@ def plot_acpc(
         )
 
     # Plot each cut axis for low-b
-    acpc_registered_img_data = acpc_registered_img.get_fdata(dtype='float32')
-    if np.all(acpc_registered_img_data <= 1e-8):
-        acpc_registered_img_cropped = acpc_registered_img
-    else:
-        acpc_registered_img_cropped = image.crop_img(acpc_registered_img)
-
+    acpc_registered_img_cropped = (
+        acpc_registered_img if crop_offset is None else acpc_registered_img.slicer[crop_offset]
+    )
     for i, mode in enumerate(list(order)):
         plot_params['display_mode'] = mode
         plot_params['cut_coords'] = [-20.0, 0.0, 20.0]