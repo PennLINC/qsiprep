0a831db9835398b81da86298a3de5fe513452980
Taylor Salo <tsalo90@gmail.com>
2026-09-11 09:25:59 -0400

    Remove `--fmap-bspline` parameter.
    This parameter had no effect.

--- qsiprep/config.py
+++ qsiprep/config.py
@@ -591,8 +591,6 @@ class workflow(_Config):
     """Configuration JSON for running TORTOISE DIFFPREP."""
     eddy_config = None
     """Configuration for running Eddy."""
-    fmap_bspline = None
-    """Regularize fieldmaps with a field of B-Spline basis."""
     fmap_demean = None
     """Remove the mean from fieldmaps."""
     force = None
